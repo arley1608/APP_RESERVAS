b768f946b7e9c55212a7cdafef56f2fc0382ff04
arley1608 <rodriguezarley8@gmail.com>
2025-03-03 22:31:58 -0500

    mejoras en screen de cada usuario

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -5,6 +5,8 @@ import 'usuarios_screen.dart';
 import 'habitaciones_screen.dart';
 import 'reservas_screen.dart';
 import 'agregar_usuario_screen.dart';
+import 'agregar_reserva_screen.dart';
+import 'ver_reservas_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
   final String rol;
@@ -52,71 +54,217 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(nombreUsuario),
+        title: Text("Bienvenido, $nombreUsuario",
+            style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Colors.white)),
+        backgroundColor: Colors.green[700],
         actions: [
           IconButton(
+            iconSize: 45,
+            icon: Icon(Icons.account_circle, color: Colors.white),
+            onPressed: () {
+              Navigator.pushNamed(context, '/perfil');
+            },
+          ),
+          IconButton(
+            iconSize: 45,
             icon: Icon(Icons.logout),
             onPressed: () => _cerrarSesion(context),
           )
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Bienvenido, $nombreUsuario", style: TextStyle(fontSize: 18)),
-            SizedBox(height: 20),
-            if (widget.rol == "admin")
-              Column(
-                children: [
-                  ElevatedButton.icon(
-                    icon: Icon(Icons.person_add),
-                    label: Text("Agregar Usuario"),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => AgregarUsuarioScreen()),
-                      );
-                    },
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.green[600]!,
+              const Color.fromARGB(255, 253, 253, 253)!
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/logo_colores.png",
+                  height: 220,
+                ),
+                SizedBox(height: 40),
+                if (widget.rol == "admin")
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => AgregarUsuarioScreen()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize:
+                              Size(MediaQuery.of(context).size.width * 0.5, 50),
+                          backgroundColor: Colors.green[700],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 40, vertical: 20),
+                        ),
+                        child: Text(
+                          "Agregar Usuario",
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => UsuariosScreen()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green[700],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 92, vertical: 20),
+                        ),
+                        child: Text(
+                          "Gestionar Usuarios",
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    HabitacionesScreen(rol: widget.rol)),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green[700],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 67, vertical: 20),
+                        ),
+                        child: Text(
+                          "Gestionar Habitaciones",
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
-                  ElevatedButton.icon(
-                    icon: Icon(Icons.supervisor_account),
-                    label: Text("Gestionar Usuarios"),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => UsuariosScreen()),
-                      );
-                    },
+                if (widget.rol == "admin" || widget.rol == "operador")
+                  Column(
+                    children: [
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          //  Navigator.push(
+                          //    context,
+                          //    MaterialPageRoute(builder: (context) => AgregarReservaScreen()),
+                          //  );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green[700],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 105, vertical: 20),
+                        ),
+                        child: Text(
+                          "Agregar Reserva",
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ReservasScreen()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green[700],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 89, vertical: 20),
+                        ),
+                        child: Text(
+                          "Gestionar Reservas",
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ElevatedButton.icon(
-              icon: Icon(Icons.hotel),
-              label: Text("Gestionar Habitaciones"),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          HabitacionesScreen(rol: widget.rol)),
-                );
-              },
+                if (widget.rol == "recepcionista")
+                  Column(
+                    children: [
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          //  Navigator.push(
+                          //    context,
+                          //    MaterialPageRoute(builder: (context) => VerReservasScreen()),
+                          //  );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green[700],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 40, vertical: 15),
+                        ),
+                        child: Text(
+                          "Ver Reservas",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+              ],
             ),
-            ElevatedButton.icon(
-              icon: Icon(Icons.book),
-              label: Text("Gestionar Reservas"),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ReservasScreen()),
-                );
-              },
-            ),
-          ],
+          ),
         ),
       ),
     );
